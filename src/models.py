ab65b5b9e89cb7f9590db5bf3113d1bc9e0cb8a1
yolandagb <yolandagarciabarranco@hotmail.com>
2021-04-26 19:01:45 +0000

    sql

--- src/models.py
+++ src/models.py
@@ -26,6 +26,49 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+class Post(Base):
+     __tablename__ = 'post'
+     id = Column(Integer, primary_key=True)
+    user_name = Column(String(250))
+
+class User(Base):
+     __tablename__ = 'user'
+     id = Column(Integer, primary_key=True)
+    user_name = Column(String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email= Column(String(250))
+
+class Follower(Base):
+     __tablename__ = 'follower'
+     id = Column(Integer, primary_key=True)
+    user_from_id = Column(String(250))  
+    user_to_id =  Column(String(250))  
+
+class User(Base):
+     __tablename__ = 'user'
+     id = Column(Integer, primary_key=True)
+    user_name = Column(String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email= Column(String(250))    
+
+class Media(Base):
+     __tablename__ = 'media'
+     id = Column(Integer, primary_key=True)
+     type = Column(String(250))
+     url = Column(String(250))
+     post_id = Column(String(250))
+    
+
+class Comment(Base):
+     __tablename__ = 'commet'
+     id = Column(Integer, primary_key=True)
+     comment_text = Column(String(250))
+     author_id= Column(String(250))
+     post_id = Column(String(250))
+        
+
     def to_dict(self):
         return {}
 
